2ce55def378e34146daa56853c4fafb8cd0b3791
smaniotov <smaniotov@gmail.com>
2018-05-16 20:02:08 -0300

    wip

--- src/main/db.js
+++ src/main/db.js
@@ -1,58 +1,76 @@
-const AWS = require("aws-sdk")
-// const dynamo = Aws.dynamo()
+const AWS = require('aws-sdk')
+const dynamo = new AWS.DynamoDB()
+const s3 = new AWS.S3()
 const fs = require("fs")
-const TableName = process.env.TableName
 
-const fillDynamo = (elements, labels, primaryLabel) => new Promise((resolve, reject) => {
-    elements.map((element, index) => {
-        if (index > 0) {
-            const [primaryValue, ...addressElem] = element.split(',')
-            const address = addressElem.reduce((out, item, index) => {
-                const label = labels[index]
-                out[label] = item
-                return out
-            } , {})
-            
-            const params = {
-                TableName,
-                Item: {
-                    [primaryLabel]: {S: primaryValue},
-                    data: {S: JSON.stringify(address)}
-                }
-            }
-            
-            dynamo.putItem(params, (err, data) => {
-                if(err) reject(err)
-                resolve(data)
-            })
+const TableName = process.env.TableName
+const bucket = process.env.S3_BUCKET
+
+const fillDynamo = (rows, labels, primaryLabel) => new Promise((resolve, reject) => {
+  rows.map((element, index) => {
+    if (index > 0) {
+      const [primaryValue, ...addressElem] = element.split('|')
+      const address = addressElem.reduce((out, item, index) => {
+        const label = labels[index]
+        out[label] = item
+        return out
+      }, {})
+
+      const params = {
+        TableName,
+        Item: {
+          'code': {
+            S: primaryValue
+          },
+          data: {
+            S: JSON.stringify(address)
+          }
         }
-    })
+      }
+
+      dynamo.putItem(params, (err, data) => {
+        if (err) reject(err)
+        resolve(data)
+      })
+    }
+  })
 })
 
-const init = () => {
-    const dataFile = fs.readFileSync(process.argv[2], "utf8")
+const loadDatabase = (fileKey) => {
+  console.log("fleKey", fileKey)
 
-    const [fileLabels, ...elements] = dataFile.split("\n")
-    const [primaryLabel, ...labels] = fileLabels.split(",").map(label => label.replace(' ', '_').toLowerCase())
+  return new Promise((resolve, reject) => {
+    s3.getObject({
+      Bucket: bucket,
+      Key: fileKey
+    }, (err, file) => {
+      console.log(file)
+      if (err) reject(err)
+      const dataFile = file.Body.toLocaleString()
 
-    const params = {
-        TableName: TableName,
-        KeySchema: [
-            {AttributeName: primaryLabel, KeyType: "HASH"}
-        ],
-        AttributeDefinitions: labels.map(label => ({AttributeName: label, AttributeType: "S"})),
-        ProvisionedThroughput: {
-            ReadCapacityUnits: 10,
-            WriteCapacityUnits: 10
-          }
-    }
+      const [header, ...rows] = dataFile.split('\n')
+      const [primaryLabel, ...labels] = header.split('|').map(label => label.replace(' ', '_').toLowerCase())
 
-    return new Promise((resolve, reject) => {
-        dynamo.createTable(params, (error, data) => {
-            if(error) reject(data)    
-            return fillDynamo(elements, labels, primaryLabel)
-        })
+      resolve(fillDynamo(rows, labels, primaryLabel))
     })
+  })
 }
 
-init()
+const index = (event, context, callback) => {
+  const fileKey = event.Records[0].s3.object.key.replace('+', ' ')
+  console.log(TableName, bucket)
+  console.log(`File ${fileKey} received`)
+  console.log(`Event: ${event}`)
+
+  if (fileKey.indexOf('.txt') >= 0) {
+    loadDatabase(fileKey)
+      .then(() => {
+        console.log('Files successfuly sended')
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+}
+
+exports.handler = index
